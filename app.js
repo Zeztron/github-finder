c52274f786ff97132f28417515a5d29fa297cf72
Zeztron <pateltharsh@gmail.com>
2018-11-12 11:42:55 -0500

    got github api and created fetch data feature

---
+++ app.js
@@ -0,0 +1,25 @@
+// initialize GitHub
+const github = new Github;
+
+// search input
+const searchUser = document.getElementById("searchUser");
+
+// search input event listener
+searchUser.addEventListener("keyup", (e) => {
+    // Get input text
+    const userText = e.target.value;
+    if(userText != "") {
+        // Make http call
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === "Not Found") {
+                // Show alert
+
+            } else {
+                // Show profile
+            }
+        })
+    } else {
+        // Clear profile
+    }
+});
